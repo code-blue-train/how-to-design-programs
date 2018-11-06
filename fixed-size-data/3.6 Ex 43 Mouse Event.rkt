e38ec79a7014adeb1c8e9232433614916f058841
Jay Heydt <jheydt@drillinginfo.com>
2018-11-05 20:16:15 -0600

    Section 3.6

--- fixed-size-data/3.6 Ex 43 Mouse Event.rkt
+++ fixed-size-data/3.6 Ex 43 Mouse Event.rkt
@@ -1,40 +1,17 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname |3.6|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |3.6 Ex 43 Mouse Event|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (require 2htdp/image)
 (require 2htdp/universe)
-; exercise 41
-; A WorldState is a Number
-; intepretation is the number of pixels between
-; the left border of the scene and the car
-
-; Wish list:
-; WorldState -> WorldState
-; for each tick of the clock, big-bang obtains the next 
-; state of the world from (clock-tick-handler cw) 
-(define (clock-tick-handler cw) ...)
- 
-; WorldState String -> WorldState 
-; for each keystroke, big-bang obtains the next state 
-; from (keystroke-handler cw ke); ke represents the key
-(define (keystroke-handler cw ke) ...)
- 
-; WorldState Number Number String -> WorldState 
-; for each mouse gesture, big-bang obtains the next state
-; from (mouse-event-handler cw x y me) where x and y are
-; the coordinates of the event and me is its description 
-(define (mouse-event-handler cw x y me) ...)
- 
-; WorldState -> Boolean
-; after each event, big-bang evaluates (end? cw) 
-;(define (end? cw) ...)
-; Wish list end
+; exercise 43
+; An AnimationState is a Number
+; intepretation is the number of clock ticks
+; since the animation started
 
 ; constants to represent the car and scene
 ; changing WHEEL-RADIUS will change the dimensions of the CAR and SCENE
 ; exercise 39
-(define WHEEL-RADIUS 5)
+(define WHEEL-RADIUS 20)
 (define WHEEL-DISTANCE (* WHEEL-RADIUS 5))
 
 (define WHEEL
@@ -79,37 +56,45 @@
 (define BACKGROUND
     (empty-scene (* WHEEL-RADIUS 40) (* WHEEL-RADIUS 6)))
 
-; WorldState -> Number
+; WorldState Number Number String -> WorldState
+; places the car at x-mouse
+; if the given me is "button-down"
+; only tested on laptop mouse pad not with a peripheral mouse
+(define (hyper as x-mouse y-mouse me)
+  x-mouse)
+
+; AnimationState -> Number
 ; stops the car when it reaches the end of the emtpy scene
-(define (end? ws)
-  (if (> ws (+ (image-width BACKGROUND) (/ (image-width CAR) 2)))
+(define (end? as)
+  (if (> as (+ (image-width BACKGROUND) (/ (image-width CAR) 2)))
       #true
       #false))
 
-; WorldState -> Image
+; AnimationState -> Image
 ; the x coordinate of the tree is the center of the BACKGROUND image
 ; the y coordinate of the tree is the bottom of the BACKGROUND image 
 ; places the image of the car x pixels from
 ; the left margin of the BACKGROUND image
-(define (render ws)
+(define (render as)
   (place-image TREE
                (/ (image-width BACKGROUND) 2)
                (-  (image-height BACKGROUND) (/ (image-height TREE) 2))
-               (place-image CAR ws Y-CAR BACKGROUND)))
+               (place-image CAR as Y-CAR BACKGROUND)))
 
-; WorldState -> WorldState
-; adds 2 to x to move the car right
+; AnimationState -> AnimationState
+; adds 2 to as to move the car on the right axis
 (check-expect (tock 0) 2)
 (check-expect (tock 21) 23)
 
-(define (tock ws)
-  (+ ws 2))
+(define (tock as)
+  (+ as 2))
 
-; WorldState -> WorldState
+; AnimationState -> AnimationState
 ; launches the program from some initial state
-(define (main ws)
-  (big-bang ws
+(define (main as)
+  (big-bang as
     [on-tick tock]
+    [on-mouse hyper]
     [stop-when end?]
     [to-draw render]
     ))
