09a776e91125d22cbdc7656dff28870b80b270f1
Jay Heydt <jheydt@drillinginfo.com>
2019-02-05 10:56:08 -0600

    Computing with functions

---
+++ abstraction/249.rkt
@@ -0,0 +1,37 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |249|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; Function Lon Number -> Lon
+; consumes a function R, list-of-numbers l and a number t
+; and produces a list-of-numbers that represents each item that
+; satisfies the condition of R when compared to t
+(check-expect (extract >
+                       (list 0 1) -1)
+              (list 0 1))
+(define (extract R l t)
+  (cond
+    [(empty? l) '()]
+    [else
+     (cond
+       [(R (first l) t)
+       (cons
+        (first l)
+        (extract R (rest l) t))]
+       [else
+        (extract R (rest l) t)])]))
+
+; Number Number -> Boolean
+; is the area of a square with side x larger than c
+(define (squared>? x c)
+  (> (* x x) c))
+
+
+; Exercise 248. Evaluate
+
+(squared>? 4 10)
+
+(> (* 4 4) 10)
+
+(> 16 10)
+
+#true
