9849d48b794bffd27eda1c5087e0879de879d1cf
Jay Heydt <jayheydt@pop-os.localdomain>
2019-04-22 11:17:18 -0500

    forests

---
+++ intertwined-data/314.rkt
@@ -0,0 +1,68 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |314|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define-struct no-parent [])
+(define-struct child [father mother name date eyes])
+
+(define NP (make-no-parent))
+; An FT is one of: 
+; – NP
+; – (make-child FT FT String N String)
+
+; Oldest Generation:
+(define Carl (make-child NP NP "Carl" 1926 "green"))
+(define Bettina (make-child NP NP "Bettina" 1926 "green"))
+ 
+; Middle Generation:
+(define Adam (make-child Carl Bettina "Adam" 1950 "hazel"))
+(define Dave (make-child Carl Bettina "Dave" 1955 "black"))
+(define Eva (make-child Carl Bettina "Eva" 1965 "blue"))
+(define Fred (make-child NP NP "Fred" 1966 "pink"))
+ 
+; Youngest Generation: 
+(define Gustav (make-child Fred Eva "Gustav" 1988 "brown"))
+
+
+; FT -> Boolean
+; does an-ftree contain a child
+; structure with "blue" in the eyes field
+ 
+(check-expect (blue-eyed-child? Carl) #false)
+(check-expect (blue-eyed-child? Gustav) #true)
+ 
+(define (blue-eyed-child? an-ftree)
+  (cond
+    [(no-parent? an-ftree) #false]
+    [else (or (string=? (child-eyes an-ftree) "blue")
+              (blue-eyed-child? (child-father an-ftree))
+              (blue-eyed-child? (child-mother an-ftree)))]))
+
+; A [List-of Structs] is one of: 
+; – '()
+; – (cons Struct [List-of Structs])
+; interpretation a family forest represents several
+; families (say, a town) and their ancestor trees
+
+(define ff1 (list Carl Bettina))
+(define ff2 (list Fred Eva))
+(define ff3 (list Fred Eva Carl))
+
+; [List-of Structs] -> Boolean
+; does the forest contain any child with "blue" eyes
+ 
+(check-expect (blue-eyed-child-in-forest? ff1) #false)
+(check-expect (blue-eyed-child-in-forest? ff2) #true)
+(check-expect (blue-eyed-child-in-forest? ff3) #true)
+ 
+(define (blue-eyed-child-in-forest? los)
+  (cond
+    [(empty? los) #false]
+    [else
+     (or (blue-eyed-child? (first los))
+         (blue-eyed-child-in-forest? (rest los)))]))
+
+(check-expect (blue-eyed-child-in-forest? ff1) (blue-eyed-child-in-forest.v2? ff1))
+(check-expect (blue-eyed-child-in-forest? ff2) (blue-eyed-child-in-forest.v2? ff2))
+(check-expect (blue-eyed-child-in-forest? ff3) (blue-eyed-child-in-forest.v2? ff3))
+(define (blue-eyed-child-in-forest.v2? los)
+  (ormap blue-eyed-child? los))
